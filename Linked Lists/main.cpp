8623e4e4d305ea3731b0fad5dc53c5ed16ce5013
Leslie Musengi <leslie.musengi@icloud.com>
2020-06-28 19:32:01 +0200

    Implemented Insert() and Display() functions

--- Linked Lists/main.cpp
+++ Linked Lists/main.cpp
@@ -13,7 +13,43 @@ struct Node{
 }*head{NULL};
 
 
+void insert(int list_elem);
+void display();
+
+
 int main(){
+    int num;
+    
+    cout << "How many numbers do you need to add?: ";
+    cin >> num;
+    
+    int list_elem;
+    for(int i{0}; i < num; i++){
+        cout << "Enter a list element: ";
+        cin >> list_elem;
+        insert(list_elem);
+        display();
+    }
   
     return 0;
+}
+
+
+void insert(int list_elem){
+    Node *last = new Node;
+    
+    last->data = list_elem;
+    last->link = head;
+    head = last;
+}
+
+void display(){
+    Node *current = head;
+    
+    cout << "Your list has the following elements: " << endl;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->link;
+    }
+    cout << endl;
 }
