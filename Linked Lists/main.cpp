400895e8f7cff0c2a326ca55238946b9d5c0a67b
Leslie Musengi <leslie.musengi@icloud.com>
2020-06-28 16:44:02 +0200

    Created a node

---
+++ Linked Lists/main.cpp
@@ -0,0 +1,17 @@
+//  Linked Lists
+//
+//  Created by Leslie Musengi on 2020/06/28.
+//  Copyright © 2020 Leslie Musengi. All rights reserved.
+
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *link;
+}*head{nullptr};
+
+int main(){
+    return 0;
+}
